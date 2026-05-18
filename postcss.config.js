1ac5e71fba95e774362aa28ccd5fe327fe56c014
Dianayuww <167867871+Dianayuww@users.noreply.github.com>
2026-05-19 04:39:44 +0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+ (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
+diff --git a/postcss.config.js b/postcss.config.js
+new file mode 100644
+index 0000000000000000000000000000000000000000..2e7af2b7f1a6f391da1631d93968a9d487ba977d
+--- /dev/null
++++ b/postcss.config.js
+@@ -0,0 +1,6 @@
++export default {
++  plugins: {
++    tailwindcss: {},
++    autoprefixer: {},
++  },
++}
+ 
+EOF
+)
